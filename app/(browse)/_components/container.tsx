697470a23f7cad2707af10b898bd1838aa744caf
Syed Rafay Jamal <57497821+Rafayj34@users.noreply.github.com>
2024-01-21 23:31:21 +0500

    Update container.tsx

--- app/(browse)/_components/container.tsx
+++ app/(browse)/_components/container.tsx
@@ -1,5 +1,6 @@
 "use client"
-
+import { useEffect } from "react";
+import { useMediaQuery } from "usehooks-ts";
 import { cn } from "@/lib/utils";
 import { useSidebar } from "@/store/use-sidebar";
 
@@ -7,7 +8,18 @@ interface ContainerProps {
     children : React.ReactNode;
 }
 export const Container = ({children}: ContainerProps) => {
-    const {collapsed} = useSidebar((state) => state)
+    const matches = useMediaQuery("(max-width: 1024px)");
+
+    const {collapsed, onCollapse, onExpand} = useSidebar((state) => state)
+
+    useEffect(() => {
+        if (matches) {
+            onCollapse();
+        } else {
+            onExpand();
+        }
+
+    }, [matches, onCollapse, onExpand])
     return (
 <div className={cn(
     "flex-1",
